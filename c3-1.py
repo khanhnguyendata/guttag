38ad4e2ee606f9d9c18ab590e48661b6e2f8fd9c
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-09-30 02:06:55 +0700

    C3-1: Extend to unbounded powers, handle unique cases (x = 0, 1)

--- c3-1.py
+++ c3-1.py
@@ -1,16 +1,36 @@
-x = 32
-root_pwr_pairs = {}
+while True:
+    x = input("Input an integer (type 'q' to quit): ")
 
-for root in range(1, abs(x)+1):
-    for pwr in range(1, 7):
-        powered = root**pwr
-        if powered == abs(x):
-            if x > 0:
-                root_pwr_pairs[root] = pwr
-                root_pwr_pairs[-root] = pwr
-            elif x < 0:
-                root_pwr_pairs[-root] = pwr
-            break
-        elif powered > abs(x):
-            break
-print(root_pwr_pairs)
+    # Handle exit
+    root_pwr_pairs = {}
+    if x.lower() == 'q':
+        break
+    else:
+        # Handle exceptions
+        try:
+            x = int(x)
+        except ValueError:
+            print("Invalid input")
+            continue
+
+        # Handle unique cases
+        if x == 0:
+            root_pwr_pairs[0] = "any power"
+        elif x == 1:
+            root_pwr_pairs[1] = "any power"
+        else:
+
+            # Handle normal cases
+            for root in range(2, abs(x)+1):
+                pwr = 1
+                while root**pwr < abs(x):
+                    pwr += 1
+                if root**pwr == abs(x):
+                    # Handle multiple roots for positive integers, and negative roots
+                    # for negative integers
+                    if x > 1:
+                        root_pwr_pairs[root] = pwr
+                        root_pwr_pairs[-root] = pwr
+                    elif x < 0:
+                        root_pwr_pairs[-root] = pwr
+        print(root_pwr_pairs)
