6ecedda4907ec2d2edf2fc99249d1db59055c7b9
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-10-11 23:02:10 +0700

    Week 3 update

---
+++ 3-gaddis_recursive_sum.py
@@ -0,0 +1,15 @@
+def main():
+    num1 = 7
+    num2 = 4
+    print(gcd(num1, num2))
+
+
+def gcd(x, y):
+    # print(x, y)
+    if x % y == 0:
+        return y
+    else:
+        return gcd(x, x % y)
+
+
+main()
