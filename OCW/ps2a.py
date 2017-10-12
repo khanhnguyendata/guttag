23d069f416f6a33c830d30541d6fa8da092d5a80
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-10-12 09:04:26 +0700

    OCW8 p2-3: Finish p2

--- OCW/ps2a.py
+++ OCW/ps2a.py
@@ -1,10 +1,7 @@
-import pprint
-
-
 def is_diophantine(x):
     """
     Input: a non-negative integer
-    Output: list containing combination of 6, 9, and 20-multiples that add up wholly to the input integer
+    Output: boolean on whether the integer is a diophantine
     """
     diophantine_flag = False
     for a in range(x//20 + 1):
@@ -18,7 +15,17 @@ def is_diophantine(x):
     return diophantine_flag
 
 
-diophantine_dict = {}
-for n in range(0, 70):
-    diophantine_dict[n] = is_diophantine(n)
-pprint.pprint(diophantine_dict)
+diophantine_count = 0
+for num in range(0, 70):
+    if is_diophantine(num):
+        diophantine_count += 1
+        print(num, diophantine_count)
+    else:
+        diophantine_count = 0
+        print(num, diophantine_count)
+
+    # if diophantine_count == 6:
+    #     break
+
+print("Largest number of McNuggets that cannot be bought in exact quantity:", num - 6)
+
