38073c9df7f57d0dcf520cd90a4dd4b2c695cf13
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-10-25 19:39:33 +0700

    Fix comments

--- 6-00-2x/1/powerset_itertools.py
+++ 6-00-2x/1/powerset_itertools.py
@@ -5,20 +5,20 @@ def powerset(items):
     """
     Generate subsets (of all lengths) of a list of items
     :param items: input list of items
-    :return: iterator that will generate subsets (in list-form) of all lengths
+    :return: iterator that generates subset (in list-form) of all lengths
     """
 
     def fixed_length_combo(items, length):
         """
         Generate subsets (of fixed length) of a list of items
-        :param items: input lists of items
+        :param items: input list of items
         :param length: length of generated subset
         :return: iterator that generates each fixed-length subset in list-form
         """
         for combo in combinations(items, length):
             yield list(combo)
 
-    # Combine all fixed-length combos into iterator that will generate each set of combo each time
+    # Combine all fixed-length combos into an iterator that will generate each set of combo each time
     variable_length_combos = (fixed_length_combo(items, i) for i in range(len(items)+1))
 
     # Chain all fixed-length combos so each subset could be queried for each fixed-length combos
