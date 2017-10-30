5a09a47e7aa4f9ef500e30e29e718bcb18fc7aab
seismatica <nguyenduykhanh.hcm@gmail.com>
2017-10-30 12:20:32 +0700

    Implement RectangularRoom class for problem set 2

---
+++ 6-00-2x/3-Graph_algorithms/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='graph_search',
+    version='1.0',
+    description='Algorithms to find shortest path in a graph',
+    author='K Nguyen',
+    author_email='nguyenduykhanh.hcm@gmail.com',
+    py_modules=['graph_data_structure'],
+)
